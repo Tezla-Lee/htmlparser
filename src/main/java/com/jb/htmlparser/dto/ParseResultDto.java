b54892617419f3ae6a090669e427fd70c9a0cb18
Tezla-Lee <tezla.lee@gmail.com>
2021-03-15 22:53:28 +0900

    feat: Update ParseResultDto
    Add fields

--- src/main/java/com/jb/htmlparser/dto/ParseResultDto.java
+++ src/main/java/com/jb/htmlparser/dto/ParseResultDto.java
@@ -1,4 +1,12 @@
 package com.jb.htmlparser.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class ParseResultDto {
+    private String quotient;
+
+    private String remainder;
 }
