8a11b54773a8f6a4836f6d70822316dfca346045
Tezla-Lee <tezla.lee@gmail.com>
2021-03-16 14:10:54 +0900

    feat: Update HtmlService
    Add boolean methods
    - 대문자 확인
    - 소문자 확인
    - 숫자 확인

--- src/main/java/com/jb/htmlparser/service/HtmlService.java
+++ src/main/java/com/jb/htmlparser/service/HtmlService.java
@@ -1,7 +1,31 @@
 package com.jb.htmlparser.service;
 
+import com.jb.htmlparser.dto.ParseResultDto;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class HtmlService {
+
+    public ParseResultDto getResult(String url, String type, Integer divisor) throws IOException {
+        ParseResultDto parseResultDto = new ParseResultDto();
+        Document doc = Jsoup.connect(url).get();
+
+        return parseResultDto;
+    }
+
+    public boolean isCapitalLetter(char c) {
+        return c >= 65 && c <= 90;
+    }
+
+    public boolean isSmallLetter(char c) {
+        return c >= 97 && c <= 122;
+    }
+
+    public boolean isNumber(char c) {
+        return c >= 48 && c <= 57;
+    }
 }
