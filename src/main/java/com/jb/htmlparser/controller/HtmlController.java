1f2bc77ff9edf906b882231d8b6a08fc1a5d67d4
Tezla-Lee <tezla.lee@gmail.com>
2021-03-16 17:32:05 +0900

    feat: Update HtmlController

--- src/main/java/com/jb/htmlparser/controller/HtmlController.java
+++ src/main/java/com/jb/htmlparser/controller/HtmlController.java
@@ -1,5 +1,32 @@
 package com.jb.htmlparser.controller;
 
+import com.jb.htmlparser.service.HtmlService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
 public class HtmlController {
-    
+
+    private final HtmlService htmlService;
+
+    @GetMapping("/")
+    public String home() {
+
+        return "/result";
+    }
+
+    @GetMapping({"/result"})
+    public String getResult(Model model, @RequestParam String url, @RequestParam String type, @RequestParam(required = false) String divisor) {
+
+        model.addAttribute("result", htmlService.getResult(url, type, divisor));
+        model.addAttribute("url", url);
+        model.addAttribute("type", type);
+        model.addAttribute("divisor", divisor);
+
+        return "/result";
+    }
 }
