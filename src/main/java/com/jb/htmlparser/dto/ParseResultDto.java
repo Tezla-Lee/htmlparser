265e95314cc0364e240b65aa775752472fdda482
Tezla-Lee <tezla.lee@gmail.com>
2021-03-16 17:30:58 +0900

    feat: Update ParseResultDto
    Add Annotations & errorMessage

--- src/main/java/com/jb/htmlparser/dto/ParseResultDto.java
+++ src/main/java/com/jb/htmlparser/dto/ParseResultDto.java
@@ -1,12 +1,20 @@
 package com.jb.htmlparser.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Data
 public class ParseResultDto {
     private String quotient;
 
     private String remainder;
+
+    @NonNull
+    private String errorMessage;
 }
